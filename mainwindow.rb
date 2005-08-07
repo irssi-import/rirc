d9a384be8012fe5f12e8bc4ff4614338ac61274a
Andrew Thompson <vagabond@cataclysm-software.net>
2005-08-07 16:50:23 +0000

    alt+num channel tabbing now works with numlock down (thanks Denstark)

--- mainwindow.rb
+++ mainwindow.rb
@@ -558,10 +558,10 @@ class MainWindow
 	end
     
     def window_buttons(widget, event)
-        puts 'Got key event' if $args['debug']
-        puts event.state.to_i if $args['debug']
+        #puts 'Got key event' if $args['debug']
+        #puts event.state.to_i if $args['debug']
         
-        if event.state == Gdk::Window::MOD1_MASK
+        if (event.state & Gdk::Window::MOD1_MASK) != 0
             puts 'pressed alt-'+Gdk::Keyval.to_name(event.keyval) if $args['debug']
             key = Gdk::Keyval.to_name(event.keyval)
             if key =~ /\d/
